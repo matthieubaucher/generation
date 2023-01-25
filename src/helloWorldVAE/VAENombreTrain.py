b711d0b67fcaf91e491462c2dfbfd48edb22c65a
matthieubaucher <103262850+matthieubaucher@users.noreply.github.com>
2023-01-25 16:51:40 +0100

    Initial commit

---
+++ src/helloWorldVAE/VAENombreTrain.py
@@ -0,0 +1,25 @@
+import numpy as np
+from tensorflow import keras
+from helloWorldVAE.VAENombre import VAE, encoder, decoder
+
+print("definition de l'espace de stockage")
+
+PATH_APPLICATION = "../../"
+PATH_RESOURCES = PATH_APPLICATION + "/resources/"
+filename = PATH_RESOURCES + 'helloWorld'
+
+print("filename = " + filename)
+
+print("chargement du dataset")
+(x_train, _), (x_test, _) = keras.datasets.mnist.load_data()
+mnist_digits = np.concatenate([x_train, x_test], axis=0)
+mnist_digits = np.expand_dims(mnist_digits, -1).astype("float32") / 255
+
+print("creation du VAE")
+vae = VAE(encoder, decoder)
+print("compilation du VAE")
+vae.compile(optimizer=keras.optimizers.Adam())
+print("entrainement du VAE")
+vae.fit(mnist_digits, epochs=30, batch_size=128)
+print("sauvegarde du VAE")
+vae.save(filename, mnist_digits)
---
+++ src/helloWorldVAE/VAEPlotLatentSpace.py
@@ -0,0 +1,29 @@
+import numpy as np
+from tensorflow import keras
+import matplotlib.pyplot as plt
+from VAENombre import Sampling, VAE
+
+
+PATH_APPLICATION = "../../"
+PATH_RESOURCES = PATH_APPLICATION + "/resources/"
+filename = PATH_RESOURCES + 'helloWorld'
+
+
+def plot_label_clusters(vae, data, labels):
+    # display a 2D plot of the digit classes in the latent space
+    z_mean, _, _ = vae.encoder.predict(data)
+    plt.figure(figsize=(12, 10))
+    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=labels)
+    plt.colorbar()
+    plt.xlabel("z[0]")
+    plt.ylabel("z[1]")
+    plt.show()
+
+
+(x_train, y_train), _ = keras.datasets.mnist.load_data()
+x_train = np.expand_dims(x_train, -1).astype("float32") / 255
+
+vae = keras.models.load_model(filename, custom_objects={'Sampling': Sampling,
+                                                        'VAE': VAE})
+
+plot_label_clusters(vae, x_train, y_train)
